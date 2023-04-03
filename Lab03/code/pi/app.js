c02a404827ff828dc97709dd321449df62079571
Matt Kaufman <98175105+makaufmanUI@users.noreply.github.com>
2023-04-03 12:38:10 -0500

    Update app.js

--- Lab03/code/pi/app.js
+++ Lab03/code/pi/app.js
@@ -30,9 +30,7 @@
 
 var nodeimu = require( '@trbll/nodeimu' );
 var sense = require( '@trbll/sense-hat-led' );
-
 var IMU = new nodeimu.IMU();
-
 var firebase = require( 'firebase/app' );
 const { getDatabase, ref, onValue, set, update, get } = require( 'firebase/database' );
 
@@ -46,8 +44,8 @@ const firebaseConfig = {
 };
 firebase.initializeApp( firebaseConfig );
 
-const database = getDatabase();
 
+const database = getDatabase();
 
 const { createBluetooth } = require( 'node-ble' );
 const ARDUINO_BLUETOOTH_ADDR = '64:AB:DF:6A:07:14';   // TODO: Replace with address obtained from 'scan on' command in bluetoothctl
@@ -55,6 +53,7 @@ const ARDUINO_BLUETOOTH_ADDR = '64:AB:DF:6A:07:14';   // TODO: Replace with addr
 const UART_SERVICE_UUID      = '6E400001-B5A3-F393-E0A9-E50E24DCCA9E';
 const TX_CHARACTERISTIC_UUID = '6E400002-B5A3-F393-E0A9-E50E24DCCA9E';
 const RX_CHARACTERISTIC_UUID = '6E400003-B5A3-F393-E0A9-E50E24DCCA9E';
+
 
 
 
@@ -121,3 +120,74 @@ main().then((ret) => {
     if (err) console.error( err );
 });
 
+
+
+
+
+
+// Callback to run on "Interval" change
+onValue(ref(database, 'Interval'), (snapshot) => 
+{
+    // Get the value of "Interval"
+    const interval = snapshot.val();
+    
+    // Forward this value to to the Arduino using the Bluetooth LE Nordic UART service profile
+});
+
+
+
+
+
+
+
+
+
+/**
+* Takes a reading from the Sense HAT's IMU and returns the humidity.
+* @returns {number} - The current humidity, or `-1` if there was an error
+*/
+function GetHumidity() {
+    var data = IMU.getValueSync();
+    var str = "[" + data.timestamp.toISOString() + "] ";
+    if (data.humidity) {
+        str += "Humidity: " + data.humidity.toFixed(4) + "%.";
+        console.log(str);
+        return data.humidity;
+    } else {
+        str += "Error getting humidity reading.";
+        console.log(str);
+        return -1;
+    }
+}
+
+
+
+
+
+/**
+* Takes a row and column number and returns the color of the pixel at that location.
+* @param {number} pixelRow - The row number of the pixel (0-7), where 0 is on the top, and 7 is on the bottom
+* @param {number} pixelColumn - The column number of the pixel (0-7), where 0 is on the left, and 7 is on the right
+* @returns {Object} color - An object with attributes `r`, `g`, and `b`, representing the RGB values of the light
+*/
+function GetPixelColor(pixelRow, pixelColumn) {
+    var color = sense.getPixel(pixelRow, pixelColumn);
+    return color;
+}
+
+
+
+
+
+/**
+* Takes a row and column number and sets the color of the pixel at that location.
+* @param {number} pixelRow - The row number of the pixel (0-7), where 0 is on the top, and 7 is on the bottom
+* @param {number} pixelColumn - The column number of the pixel (0-7), where 0 is on the left, and 7 is on the right
+* @param {number} red - The red value of the pixel (0-255)
+* @param {number} green - The green value of the pixel (0-255)
+* @param {number} blue - The blue value of the pixel (0-255)
+* @returns {void}
+*/
+function SetPixelColor(pixelRow, pixelColumn, red, green, blue) {
+    sense.setPixel(pixelRow, pixelColumn, red, green, blue);
+}
