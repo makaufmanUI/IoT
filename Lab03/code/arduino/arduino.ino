90958b04ec9eefc7bc1401cf1c8784ab9864a89e
Matt Kaufman <98175105+makaufmanUI@users.noreply.github.com>
2023-04-03 12:19:34 -0500

    Update arduino.ino

--- Lab03/code/arduino/arduino.ino
+++ Lab03/code/arduino/arduino.ino
@@ -19,6 +19,18 @@
 *   For the purposes of this lab, the Arduino board acts as a peripheral device, advertising a UART service over BLE,
 *   and the Raspberry Pi acts as a central device, allowing the exchange of data between the two using the defined characteristics of the UART service.
 *
+*
+*   Specific requirements of the Arduino sketch:
+*       1. Read the temperature from the HTS221 sensor used in Lab 1 and transmit it to the Pi via
+*          the Temperature characteristic of the Environmental Sensing service at the Interval specified by the Pi.
+*          DO NOT use the UART service to transmit temperature data.
+*       2. Receive the sampling interval from the Pi via the Bluetooth LE Nordic UART
+*          service profile and change the sampling interval of the temperature sensor accordingly.
+*       3. Print the following messages to the serial monitor (at a minimum):
+*              a) The temperature after every Interval’s reading.
+*              b) The new Interval every time it is received from the Pi.
+*                 This message should be printed within one second of the Pi sending the new interval.
+*
 *********************/
 
 
