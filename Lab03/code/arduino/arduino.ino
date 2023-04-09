a36046841080299c644905252cd037decd2f8f03
Matt Kaufman <98175105+makaufmanUI@users.noreply.github.com>
2023-04-08 19:31:58 -0500

    Update arduino.ino

--- Lab03/code/arduino/arduino.ino
+++ Lab03/code/arduino/arduino.ino
@@ -131,15 +131,16 @@ void loop()
             if (txChar.written())
             {
                 // Check if it's the Interval value from the central device
-//                 if (txChar.value() == ...)
-//                 {
-//                     firebaseInterval = txChar.value();                  // Update global delay timer
-//                     Serial.print(">> [Recv] New `Interval` value: ");   // Print new Interval value to console
-//                     Serial.println(txChar.value());
-//                 } else {
-//                     Serial.print(">> [Recv] ");
-//                     Serial.println(txChar.value());
-//                 }
+                if (txChar.value() == ...)
+                {
+                    Serial.print(">> [Recv] New `Interval` value: ");   // Print new Interval value to console
+                    Serial.println(txChar.value());
+                    firebaseInterval = txChar.value();                  // Update global delay timer
+                    Serial.println(">> Updated temperature sensor sampling rate.");
+                } else {
+                    Serial.print(">> [Recv] ");
+                    Serial.println(txChar.value());
+                }
                 Serial.print(">> [Recv] ");
                 Serial.println(txChar.value());
             }
