406d7317c6c94c427506aab56565b012067f9ed7
Matt Kaufman <98175105+makaufmanUI@users.noreply.github.com>
2023-04-24 13:28:40 -0500

    Create transmitter.py

---
+++ FinalProject/RF/transmitter.py
@@ -0,0 +1,11 @@
+"""
+# transmitter.py
+
+Created by:  Matt Kaufman
+Date:        April 21, 2023
+
+Code for the transmitter (TX118SA-4) of the 433MHz RF transmitter/receiver pair.
+Cyclically emits a 433MHz signal on the K1-K4 pins by pulling them LOW (nominally +VCC).
+"""
+
+...
