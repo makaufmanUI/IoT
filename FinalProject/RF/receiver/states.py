bd43d7971ac3caf591e97b17aecc8ac10ee8ba73
Matt Kaufman <98175105+makaufmanUI@users.noreply.github.com>
2023-04-26 07:39:36 -0500

    Create states.py

---
+++ FinalProject/RF/receiver/states.py
@@ -0,0 +1,12 @@
+"""
+# states.py
+
+Created by:  Matt Kaufman
+Date:        April 26, 2023
+
+Code for the receiver (RX480E-4) of the 433MHz RF transmitter/receiver pair.
+Defines the `States` class, which represents the states of the pins corresponding to North, South, East, and West.
+"""
+
+from datetime import datetime
+import matplotlib.pyplot as plt
