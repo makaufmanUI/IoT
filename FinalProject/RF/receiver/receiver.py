d1f395c6f4b774b3e61d53d611947661f54902cf
Matt Kaufman <98175105+makaufmanUI@users.noreply.github.com>
2023-04-26 07:37:07 -0500

    Create receiver.py

---
+++ FinalProject/RF/receiver/receiver.py
@@ -0,0 +1,16 @@
+"""
+# receiver.py
+
+Created by:  Matt Kaufman
+Date:        April 26, 2023
+
+Code for the receiver (RX480E-4) of the 433MHz RF transmitter/receiver pair.
+Continuously checks for an incoming signal from the transmitter (TX118SA-4),
+and makes the distinction between channels that the incoming signal was received on.
+"""
+
+from time import sleep
+import RPi.GPIO as GPIO
+from typing import Callable
+from datetime import datetime
+import matplotlib.pyplot as plt
